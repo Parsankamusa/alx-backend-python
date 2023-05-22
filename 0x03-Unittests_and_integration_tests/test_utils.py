a67e4aba4c8808659df455e91955f6120cd7b7c6
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-22 20:14:47 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_acces_nested_map(self, nested_map, path, answer):
+    def test_access_nested_map(self, nested_map, path, answer):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), answer)
 
@@ -62,7 +62,7 @@ class TestMemoize(unittest.TestCase):
                 """ property """
                 return self.a_method()
         with patch.object(TestClass, "a_method") as mockMethod:
-            test_case = TestClass()
+            test_case = TestCase()
             test_case.a_property
             test_case.a_property
             mockMethod.assert_called_once
