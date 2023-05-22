1a963601842977391c8a78a02277a12698e55f8d
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-22 20:08:38 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class Test_Access_Nested_Map(unittest.TestCase):
     """ TESTCASE """
     """ to test the function for following inputs """
     @parameterized.expand([
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, answer):
+    def test_nested_map(self, nested_map, path, answer):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), answer)
 
@@ -23,7 +23,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_nested_map_exception(self, nested_map, path):
         """ method to test that a KeyError is raised properly """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
