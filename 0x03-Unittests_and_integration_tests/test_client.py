de968be39793ca4d109cca91ed980a5dddfce266
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-23 00:36:50 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" Module for testing client """
 from client import GithubOrgClient
 from fixtures import TEST_PAYLOAD
 from parameterized import parameterized, parameterized_class
